0a60205de0594368351669755068049db3629ef5
91087957 <91087957@mvusd.net>
2017-09-18 10:15:05 -0700

    finishing houses.java

--- src/org/teachingkidsprogramming/section02methods/Houses_Pointy.java
+++ src/org/teachingkidsprogramming/section02methods/Houses_Pointy.java
@@ -1,9 +1,9 @@
-package org.teachingkidsprogramming.section02methods.Variations;
+package org.teachingkidsprogramming.section02methods;
 
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
 
-public class Houses2
+public class Houses_Pointy
 {
   public static void main(String[] args)
   {
@@ -11,18 +11,27 @@ public class Houses2
     Tortoise.setSpeed(10);
     Tortoise.setX(200);
     int height = 40;
-    drawHouse(height);
-    drawHouse(120);
-    drawHouse(90);
-    drawHouse(20);
+    drawHouseFlatRoof(height);
+    drawHousePointyRoof(120);
+    //drawHouse(90);
+    //drawHouse(20);
   }
-  private static void drawHouse(int height)
+  private static void drawHousePointyRoof(int height)
   {
     Tortoise.setPenColor(Reds.Red);
     Tortoise.move(height);
-    //drawFlatRoof();
-    //draw pointy roof
     drawPointyRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawHouseFlatRoof(int height)
+  {
+    Tortoise.setPenColor(Reds.Red);
+    Tortoise.move(height);
+    drawFlatRoof();
+    //drawPointyRoof();
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
