2817f82f84e6a4b55f261ff91b2a3e1881944da4
91087957 <91087957@mvusd.net>
2017-10-02 10:28:21 -0700

    edited HiLow2.java

---
+++ src/org/teachingkidsprogramming/section03ifs/HiLow2.java
@@ -0,0 +1,37 @@
+package org.teachingkidsprogramming.section03ifs;
+
+import org.teachingextensions.logo.Sound;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
+public class HiLow2
+{
+  public static void main(String[] args)
+  {
+    int answer = (int) (Math.random() * 100 + 1);
+    // System.out.println(" The answer is " + answer); 
+    //Random
+    for (int i = 1; i <= 8; i++)
+    {
+      int guess = MessageBox.askForNumericalInput("What is your guess ? ");
+      if (guess == answer)
+      {
+        Sound.playBeep();
+        MessageBox.showMessage("You Won !");
+        break;
+      }
+      else if (guess > answer)
+      {
+        MessageBox.showMessage("Too high");
+        MessageBox.showMessage(" You have 8 guesses left");
+      }
+      else
+      {
+        MessageBox.showMessage("Too low");
+      }
+      if (i == 8)
+      {
+        MessageBox.showMessage("You lost !");
+      }
+    }
+  }
+}
