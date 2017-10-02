4ec0b1c96fbceca626c78c7aa066ad5d7f417c19
91087957 <91087957@mvusd.net>
2017-10-02 10:38:22 -0700

    added HiLow3.java

--- src/org/teachingkidsprogramming/section03ifs/HiLow3.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow3.java
@@ -3,16 +3,22 @@ package org.teachingkidsprogramming.section03ifs;
 import org.teachingextensions.logo.Sound;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
-public class HiLow2
+public class HiLow3
 {
   public static void main(String[] args)
   {
     int answer = (int) (Math.random() * 100 + 1);
     // System.out.println(" The answer is " + answer); 
     //Random
+    int upperRange = 100;
     for (int i = 1; i <= 8; i++)
     {
       int guess = MessageBox.askForNumericalInput("What is your guess ? ");
+      while (guess < 1 || guess > 100)
+      {
+        MessageBox.showMessage("Pleasee enter a valid guess between 1 and " + upperRange);
+        guess = MessageBox.askForNumericalInput("What is your guess ? ");
+      }
       if (guess == answer)
       {
         Sound.playBeep();
