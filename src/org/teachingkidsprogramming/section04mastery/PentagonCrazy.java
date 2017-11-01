43be49531ddb0df8a9955f1cdad76adce2a459c2
91087957 <91087957@mvusd.net>
2017-11-01 08:23:44 -0700

    finished PentagonCrazy

--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -23,8 +23,6 @@ public class PentagonCrazy
     {
       //
       adjustPen();
-      //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
-      //        Move the tortoise the length of a side --#4.1
       int length = i;
       Tortoise.move(length);
       Tortoise.turn(360 / 5);
@@ -34,18 +32,20 @@ public class PentagonCrazy
   private static void adjustPen()
   {
     Tortoise.setPenColor(ColorWheel.getNextColor());
-    //        Increase the tortoises pen width by 1 --#15                                              
+    Tortoise.setPenWidth(Tortoise.getPenWidth() + 1);
     //        If the tortoise's pen width is greater than 4, then --#17
-    //            Reset the pen width to 1 --#16
+    if (Tortoise.getPenWidth() > 4)
+    {
+      Tortoise.setPenWidth(1);
+    }
     //
   }
   private static void createColorPalette()
   {
     ColorWheel.addColor(Blues.SteelBlue);
-    //    Add dark orchid to the color wheel --#11
     ColorWheel.addColor(PenColors.Purples.DarkOrchid);
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
+    ColorWheel.addColor(PenColors.Blues.DarkSlateBlue);
+    ColorWheel.addColor(PenColors.Blues.Teal);
+    ColorWheel.addColor(PenColors.Purples.Indigo);
   }
 }
