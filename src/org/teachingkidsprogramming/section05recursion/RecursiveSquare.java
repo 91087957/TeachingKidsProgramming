fa65be99c434c237e0ef5526ad64140ae2019c92
91087957 <91087957@mvusd.net>
2017-11-14 09:59:51 -0800

    worked on recursivesquare

--- src/org/teachingkidsprogramming/section05recursion/RecursiveSquare.java
+++ src/org/teachingkidsprogramming/section05recursion/RecursiveSquare.java
@@ -12,6 +12,7 @@ public class RecursiveSquare
     Tortoise.setPenColor(PenColors.Yellows.Gold);
     //
     //  Set the speed to the fastest --#8
+    Tortoise.setSpeed(10);
     //  Update the length to 100.0 --#1.1
     double length = 100.0;
     //  MakeASquare with the current length(recipe below) --#11.4
@@ -21,19 +22,14 @@ public class RecursiveSquare
     //      Run the recipe moveToTheSquareStart with the current length  --#4.3
     //
     //      Create the moveToTheSquareStart recipe  --#4.1
-    Tortoise.setPenUp();
-    Tortoise.move(length / 2);
-    Tortoise.turn(-90);
-    Tortoise.move(length / 2);
-    Tortoise.turn(180);
-    Tortoise.setPenDown();
-    //      End of moveToTheSquareStart recipe  --#4.2
+    moveToTheSquareStart(length);
     //
     //  Do the following 4 times --#7.1
     //    Move the Tortoise the current length --#6.2
     //    MakeASquare with the current length divided by 1.7 (recipe below)--#11.3 
     //      If the current process count is less than 3 (HINT: use 'i') --#9
     //          Turn the tortoise 90 degrees to the right --#6.1
+    Tortoise.turn(90);
     //  End Repeat --#7.2
     //
     //  MoveBackToCenter with the current length (recipe below)--#5.3
@@ -52,5 +48,16 @@ public class RecursiveSquare
     //  Set the current length to the current length times two --#10.1
     //
     //  End of makeASquare recipe --#11.2
+  }
+
+  private static void moveToTheSquareStart(double length)
+  {
+    Tortoise.setPenUp();
+    Tortoise.move(length / 2);
+    Tortoise.turn(-90);
+    Tortoise.move(length / 2);
+    Tortoise.turn(180);
+    Tortoise.setPenDown();
+    //      End of moveToTheSquareStart recipe  --#4.2
   }
 }
