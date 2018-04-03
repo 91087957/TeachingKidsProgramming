70450e95757b81c9de59ead0efeecb55c930054a
91087957 <91087957@mvusd.net>
2018-04-03 09:16:28 -0700

    editing

---
+++ src/org/teachingkidsprogramming/section09final/pow.java
@@ -0,0 +1,13 @@
+package org.teachingkidsprogramming.section09final;
+
+public class pow
+{
+  public static void main(String[] args)
+  {
+    int base = 2;
+    int exponent = 3;
+    int result = pow(base, exponent);
+    System.out.println("the answer is: " + result);
+  }
+public static void bb
+}
---
+++ src/org/teachingkidsprogramming/section09final/pow3.java
@@ -0,0 +1,16 @@
+package org.teachingkidsprogramming.section09final;
+
+public class pow3
+{
+ public static void main(String [] args)
+ {
+   int base = ; 
+   int exp = ;
+   int result = pow(base, exp);
+   System.out.println("the answer is : " + );
+ }
+  public int pow(int base, int exp)
+  {
+    int product = 1;
+  }
+}// end of class
